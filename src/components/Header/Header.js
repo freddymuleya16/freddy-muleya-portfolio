ba5c6ba5c2b8ac8e8b5252e931c9c199b5df2299
freddymuleya16 <80630982+freddymuleya16@users.noreply.github.com>
2021-11-23 10:18:00 +0200

    Changed color schemes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,5 @@
 import Link from "next/link";
 import React from "react";
-import {
-  AiFillFacebook,
-  AiFillGithub,
-  AiFillInstagram,
-  AiFillLinkedin,
-  AiFillPhone,
-  AiFillTwitterCircle,
-} from "react-icons/ai";
 import { DiCssdeck } from "react-icons/di";
 import Socials from "../Socials/Socials";
 
@@ -26,24 +18,24 @@ const Header = () => (
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: "white",marginBottom:"20px" }}>
           <DiCssdeck size="3em" />
-          <Span>Portfolio</Span>
+          <Span>Freddy's Portfolio</Span>
         </a>
       </Link>
     </Div1>
     <Div2>
       <li>
-        <Link href="#projects">
-          <NavLink>Projects</NavLink>
+        <Link href="#projects" >
+          <NavLink style={{ display: "flex",marginTop:"10px" }}>Projects</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
+        <Link href="#tech" >
+          <NavLink style={{ display: "flex",marginTop:"10px" }}>Technologies</NavLink>
         </Link>
       </li>
       <li>
         <Link href="#about">
-          <NavLink>About</NavLink>
+          <NavLink style={{ display: "flex",marginTop:"10px" }}>About</NavLink>
         </Link>
       </li>
     </Div2>
