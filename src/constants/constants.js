6542ebf64439a2f9030576a0d4754bbd2711a313
Freddy Muleya <freddymuleya16@gmail.com>
2023-08-24 15:57:46 +0200

    Update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,16 @@
 export const projects = [
   {
-    title: 'Radio APP',
-    description: "Using Php, MySql, Jquery & MVC I built an MVC application for Radio, where a user can listen to admin's playlist, like and comment",
+    title: 'Radio Streaming Platform',
+    description: "As part of this project, I developed a robust MVC application for an online radio streaming platform. The application was built using a stack comprising PHP, MySQL, jQuery, and followed the Model-View-Controller (MVC) architectural pattern. Users can seamlessly access and enjoy playlists curated by the platform's admin. Additionally, interactive features such as liking and commenting on the content enhance user engagement.",
     image: '/images/radio.png',
-    tags: ['Php', 'MySql', 'Jquery', 'MVC'],
+    tags: ['PHP', 'MySQL', 'jQuery', 'MVC'],
     source: 'https://github.com/freddymuleya16/RadioApp',
     visit: '#',
     id: 0,
   },
   {
-    title: 'Freddy Muleya Portfolio',
-    description:"This is my portfolio website built using React and javascript",
+    title: 'Freddy Muleya',
+    description: "My personal portfolio website, constructed using React and JavaScript, serves as a dynamic showcase of my diverse projects and technical expertise. By leveraging React's capabilities, the website offers an interactive and engaging experience for visitors, allowing them to explore my journey, skills, and accomplishments in an immersive manner.",
     image: '/images/portfolio.png',
     tags: ['React', 'JavaScript'],
     source: 'https://github.com/freddymuleya16/freddy-muleya-portfolio',
@@ -18,25 +18,63 @@ export const projects = [
     id: 1,
   },
   {
-    title: 'Freddy\'s Riddles ',
-    description: "An android Riddle quiz built using Android Kotlin and firebase",
+    title: 'Freddy\'s Riddles: Interactive Quiz App',
+    description: "A captivating Android application named 'Freddy's Riddles' was crafted using Kotlin programming language and integrated with Firebase for robust backend functionality. The app offers a collection of mind-bending riddles that engage users in an intellectually stimulating quiz experience. With intuitive navigation and an attractive user interface, this app challenges users' problem-solving skills while providing an entertaining and educational platform.",
     image: '/images/Riddles.jpg',
     tags: ['Kotlin', 'Firebase'],
     source: 'https://github.com/freddymuleya16/Freddyriddles',
     visit: '#',
     id: 2,
   },
+  {
+    title: 'SalonSpot: Salon Booking App',
+    description: "With SalonSpot, I engineered a state-of-the-art salon booking application using React Native and harnessed the prowess of Firebase for seamless backend operations. The integration of Styled Components ensures an elegant and intuitive user experience, empowering users to effortlessly schedule appointments and manage their salon visits.",
+    image: '/images/salonspot.png',
+    tags: ['React Native', 'Firebase', 'Styled Components'],
+    source: '#',
+    visit: '#',
+    id: 3,
+  },
+  {
+    title: 'Turf Wars Vengeance: Fantasy League Aggregator Website',
+    description: "I embarked on an exhilarating journey to create Turf Wars Vengeance, a fantasy league aggregator website that converges football enthusiasts. The tech stack boasts the power of Tailwind CSS, Next.js, Firebase, and the FPL API. Football fanatics can revel in the consolidated league data and enhance their fantasy league experience.",
+    image: '/images/turfwars.png',
+    tags: ['Tailwind CSS', 'Next.js', 'Firebase', 'FPL API'],
+    source: '#',
+    visit: 'https://turf-wars-vengeance.vercel.app/',
+    id: 4,
+  },
+  {
+    title: 'Mingle Dating Website',
+    description: "As part of the Mingle project, I envisioned and crafted a sophisticated dating website that leverages the capabilities of Tailwind CSS and Styled Components to create an aesthetically pleasing and user-friendly interface. The integration with Firebase ensures robust backend functionalities, while Next.js boosts performance, rendering a seamless and enjoyable user experience.",
+    image: '/images/mingle-website.png',
+    tags: ['Tailwind CSS', 'Firebase', 'Next.js'],
+    source: '#',
+    visit: 'https://mingle-sa.vercel.app/',
+    id: 5,
+  },
+  {
+    title: 'Mingle Dating App',
+    description: "Complementing the Mingle ecosystem, the React Native app offers a dynamic and engaging dating experience for mobile users. By employing the power of Styled Components and Firebase, the app delivers real-time interactions and meaningful connections. The fusion of technology and user-centric design results in an app that connects hearts effortlessly.",
+    image: '/images/mingle-app.png',
+    tags: ['React Native', 'Styled Components', 'Firebase'],
+    source: '#',
+    visit: '#',
+    id: 6,
+  },
 ];
 
 export const TimeLineData = [
-  { year: 2018, text: 'Started my journey', },
-  { year: 2019, text: 'Developed Radio App and managed to deploy it live', },
-  { year: 2020, text: 'Started android development and managed to develop Freddy\'s Riddles', },
-  { year: 2021, text: 'Started at eGrowth as a Software developer', }
+  { year: 2018, text: 'Embarked on the Exciting Journey', },
+  { year: 2019, text: 'Brought the Radio App to Life and Successfully Deployed', },
+  { year: 2020, text: 'Ventured into Android Development, Giving Rise to Freddy\'s Riddles', },
+  { year: 2021, text: 'Commenced an Empowering Chapter at eGrowth as a Software Developer', },
+  { year: 2022, text: 'Contributed to Bespoke Software Development at Fullstack', },
+  { year: 2023, text: 'Embarked on a Freelancing Odyssey, Crafting Websites for Turf Wars Vengeance, Mingle Dating, and SalonSpot', }
 ];
 
 export const Achievementdata = [
-  { number: 3, text: 'Open Source Projects'},
-  { number: 6, text: 'Months experience in .Net stack', },
-  { number: 3, text: 'Years as Web developer', }
+  { number: 3, text: 'Contributions to Open Source Projects'},
+  { number: 2, text: 'Years of Expertise in the .NET Stack', },
+  { number: 3, text: 'Years Dedicated to Web Development', }
 ];
